32413bacf079e8e3e572144102179f93c4e93cd8
FlamesAlejandro <flameskazu@gmail.com>
2024-12-30 00:12:19 -0300

    config :wrench:: edited script, added more type of commits

--- .husky/scripts/prepare-commit-msg.js
+++ .husky/scripts/prepare-commit-msg.js
@@ -3,15 +3,39 @@ const path = require('path');
 
 // Definición de los tipos de commit y sus íconos
 const commitIcons = {
-  feature: ':sparkles:',
-  fix: ':bug:',
-  docs: ':memo:',
-  style: ':art:',
-  refactor: ':hammer:',
-  test: ':rotating_light:',
-  chore: ':wrench:',
-  perf: ':zap:',
-  misc: ':question:'
+    init: ':tada:',
+    feat: ':sparkles:',
+    feature: ':sparkles:',
+    chore: ':wrench:',
+    docs: ':memo:',
+    fix: ':bug:',
+    bugfix: ':bug:',
+    hotfix: ':ambulance:',
+    perf: ':zap:',
+    performance: ':zap:',
+    refactor: ':hammer:',
+    test: ':rotating_light:',
+    cosmetic: ':lipstick:',
+    style: ':gem:',
+    remove: ':fire:',
+    cicd: ':green_heart:',
+    CICD: ':green_heart:',
+    security: ':lock:',
+    lint: ':shirt:',
+    tslint: ':necktie:',
+    build: ':hammer_and_wrench:',
+    version: ':bookmark:',
+    release: ':robot:',
+    deployment: ':rocket:',
+    wip: ':construction:',
+    removeDependency: ':heavy_minus_sign:',
+    addDependency: ':heavy_plus_sign:',
+    docker: ':whale:',
+    config: ':wrench:',
+    package: ':package:',
+    solveConflicts: ':rage:',
+    codeSmells: ':nose:',
+    misc: ':question:'
 };
 
 // Archivo de mensaje de commit
