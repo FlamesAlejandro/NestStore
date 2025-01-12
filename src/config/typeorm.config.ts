1e56afc85589d52fcd1b55c6dc3e65b923eed123
FlamesAlejandro <flameskazu@gmail.com>
2025-01-12 19:48:36 -0300

    fix :bug:: dockercompose fix and mysql fix name

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,12 +3,13 @@ import { DataSourceOptions } from 'typeorm'
 
 export const typeOrmConfig: TypeOrmModuleOptions & DataSourceOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'digital_store',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: process.env.DB_SYNC === 'true',
-  logging: true
+  host: 'mysql',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'digital_store',
+  autoLoadEntities: true,
+  synchronize: true, // Solo en desarrollo
+  retryAttempts: 10, // Reintenta hasta 10 veces
+  retryDelay: 3000 // Espera 3 segundos entre intentos
 }
